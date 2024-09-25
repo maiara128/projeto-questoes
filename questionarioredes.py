9aa41b061a31972aa5c9b7ef68c6a52ae2bf6317
Maiara <alunamaiaralopes@gmail.com>
2024-09-25 08:01:45 -0300

    first commit

---
+++ questionarioredes.py
@@ -0,0 +1,73 @@
+print ('Bem vindos ao questionario sobre redes. escolha a alternativa correta')
+
+print ('Vamos começar. boa sorte!!')
+
+print ('O que é fibra óptica?')
+print ('a- As fibras ópticas são filamentos transparentes capazes de refletir luz por diversas vezes em seu interior')
+print ('b- Tipo de cabo que não tem uma longa vida útil')
+print ('c- É um nucleo tranparente')
+
+resposta = input ('dígite uma das opções')
+
+if resposta == 'a': 
+     print ('resposta correta')
+if resposta == 'b':
+     print ('resposta errada')
+if resposta == 'c': 
+     print ('resposta errada')
+
+print ('Sobre máscara de sub-rede assinale a resposta correta')
+print ('a- É um número de 82 bits utilizados para separar os endereços IP em partes de rede e host')
+print ('b- Ela não evita sobrecarga nos servidores e na rede em si')
+print ('c- É um número de 32 bits utilizados para separar os endereços IP em partes de rede e host')
+
+resposta = input ('dígite uma das opções')
+
+if resposta == 'a': 
+     print ('resposta errada')
+if resposta == 'b': 
+     print ('resposta errada')
+if resposta == 'c': 
+     print ('resposta correta')
+
+print ('Cabo de rede cat 8')
+print ('a- Suporta taxas de 10 Gb/s em comprimentos de até 100 metros')
+print ('b- Suporta conexões de 1 Gb/s em comprimentos de até 100 metros ou de 10 Gb/s até 55 metros')
+print ('c- Suporta taxas de 25 ou 40 Gb/s e frequência de até 2.000 MHz')
+
+resposta = input ('dígite uma das opções')
+
+if resposta == 'a': 
+     print ('resposta errada')
+if resposta == 'b':
+     print ('resposta errada')
+if resposta == 'c': 
+     print ('resposta correta')
+
+print ('Qual o nome do alicate usado para fazer conectores?')
+print ('a- Alicate de conexão')
+print ('b- Alicate de crimpar')
+print ('c- Alicate de grimpar')
+
+resposta = input ('dígite uma das opções')
+
+if resposta == 'a': 
+     print ('resposta errada')
+if resposta == 'b':
+     print ('resposta correta')
+if resposta == 'c':
+     print ('resposta errada')
+     
+print ('Assinal a alternativa correta sobre Half-duplex')
+print ('a- É a verdadeira comunicação bidirecional. A e B podem transmitir e receber dados ao mesmo tempo')
+print ('b- Nesse tipo de dados um dispositivo é o transmissor e o outro é o receptor. a transmissão de dados é simples, portanto, unidirecional')
+print ('c- É birecional, mas por compartilharem o mesmo canal de comunicação, os dispositivos não transmitem e recebem dados ao mesmo tempo')
+
+resposta = input ('dígite uma das opções')
+
+if resposta == 'a':
+     print ('resposta errada')
+if resposta == 'b':
+     print ('resposta errada')
+if resposta == 'c':
+     print ('resposta correta')
